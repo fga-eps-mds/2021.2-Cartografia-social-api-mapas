3c7bd1f928971be1cb9f212fbddace898eaf6b40
Elias Bernardo <ebmm01@gmail.com>
2021-09-26 18:33:48 -0300

    [#12] feat - add area schema and register it in maps modules
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>

---
+++ src/mapas/entities/area.schema.ts
@@ -0,0 +1,33 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type AreaDocument = Area & Document;
+
+@Schema()
+export class Area {
+  @Prop()
+  title: string;
+
+  @Prop()
+  description?: string;
+
+  @Prop({ type: String })
+  type = 'Polygon';
+
+  @Prop()
+  coordinates: number[][][];
+}
+
+export const AreaSchema = SchemaFactory.createForClass(Area);
+
+AreaSchema.virtual('id').get(function () {
+  return this._id.toHexString();
+});
+
+AreaSchema.set('toJSON', {
+  virtuals: true,
+  transform: function (doc, ret) {
+    delete ret._id;
+    delete ret.__v;
+  },
+});
--- src/mapas/mapas.module.ts
+++ src/mapas/mapas.module.ts
@@ -3,10 +3,12 @@ import { MapasService } from './mapas.service';
 import { MapasController } from './mapas.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Point, PointSchema } from './entities/point.schema';
+import { Area, AreaSchema } from './entities/area.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Point.name, schema: PointSchema }]),
+    MongooseModule.forFeature([{ name: Area.name, schema: AreaSchema }]),
   ],
   controllers: [MapasController],
   providers: [MapasService],
