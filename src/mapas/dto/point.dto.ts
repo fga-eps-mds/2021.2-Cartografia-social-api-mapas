da5c277433be351fb0add857f8d8645ba7ebadd6
Guilherme Guy <guilhermeguy349@gmail.com>
2021-10-15 09:42:26 -0300

    [#21] refat - Add missing field to DTOs
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

--- src/mapas/dto/point.dto.ts
+++ src/mapas/dto/point.dto.ts
@@ -5,6 +5,7 @@ export class PointDto {
   static convertFromPointDocument(point: PointDocument) {
     const newPointDto = new PointDto();
 
+    newPointDto.id = point.id;
     newPointDto.title = point.title;
     newPointDto.description = point.description;
     newPointDto.coordinates = point.coordinates;
@@ -13,6 +14,7 @@ export class PointDto {
   }
 
   // these values will be initialized from a PointDocument
+  id: string;
   title: string;
   description?: string;
   type = 'Point';
